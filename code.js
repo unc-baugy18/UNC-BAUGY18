eead5cb64b93bbd0b436f9d0afb8f6512b9f0836
MaxBird18-le-vrai <million.maxime18@gmail.com>
2025-09-01 22:36:01 +0200

    Ajout de styles pour les cartes photo et mise à jour de la logique d'affichage des événements dans le script

--- code.js
+++ code.js
@@ -1,4 +1,4 @@
-const sheetURL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSztBiOrLFMqZs_7g2TGdM1UxlnKoTbO7WtaQdFiODdqNe9YcVWr_rZx7ojWIqTKzychK_i1DohWD1w/pub?output=csv"
+const sheetURL = "..."
 const proxyURL = `https://api.allorigins.win/get?url=${encodeURIComponent(sheetURL)}`;
 var globalSheetData;
 // Dans loadSheetData, après avoir rempli globalSheetData :
@@ -7,6 +7,7 @@ async function fetchSheetData() {
   try {
     const response = await fetch(proxyURL);
     const data = await response.json();
+    console.log(data);
     const sheetContent = data.contents;
     const base64Match = sheetContent.match(/data:text\/csv;base64,([^"]+)/);
     let decodedCSV = base64Match && base64Match[1] ? atob(base64Match[1]) : sheetContent;
@@ -26,7 +27,7 @@ function parseCSV(csv) {
       result.push({
         year: line[0].trim(),  // Première colonne = année
         event: line[1].trim(),  // Deuxième colonne = événement
-        folder: line[3].trim().replace('/view?usp=drive_link', '/preview'),  // Quatrième colonne = dossier
+        folder: line[3].trim(),  // Quatrième colonne = dossier
         default: line[5].trim()  // Sixième colonne = image par défaut
       });
     }
@@ -66,18 +67,15 @@ fetchSheetData().then(() => {
         //     .filter(item => Number(item.year) === date)
         //     .map(item => item.event))];
 
-        const events = {};
+        const eventsWithDefaultImages = {};
 
         for (const row of globalSheetData) {
-            if (Number(row.year) === date) {
-                if (!(row.event in events)){
-                    events[row.event] = "";
-                }
-                if (row.default === "x") {
-                    events[row.event] = row.folder;
-                }
+            if (row[5] === "x") {
+                eventsWithDefaultImages[row[0]] = row[3];
             }
         }
+
+        console.log(eventsWithDefaultImages);
 
         document.getElementsByClassName('bouton-retour')[0].hidden = false;
         document.getElementById('back').addEventListener('click', () => {
@@ -88,21 +86,14 @@ fetchSheetData().then(() => {
         document.getElementById('events-list').innerHTML = '';
         document.getElementById('folder-list').innerHTML = '';
         
-        for (const [evennement, imageLink] of Object.entries(events)) {
+        for (const evennement of events) {
             const eventDiv = document.createElement('a');
             eventDiv.href = "";
             eventDiv.classList.add('card-role','photo');
-            if (imageLink === "") {
-                eventDiv.innerHTML = `
-                <img class="card-image" src="https://placehold.co/90x90/aabbcc/ffffff?text=${evennement}" alt="">
-                <h3 class="card-title">${evennement}</h3>`
-            } else {
-                eventDiv.innerHTML = `
-                <div>
-                    <iframe class="card-image" src="${imageLink}" scrolling="no" frameborder='0' alt=""></iframe>
-                </div>
-                <h3 class="card-title">${evennement}</h3>`
-            }
+            eventDiv.innerHTML = `
+            <img class="card-image" src="https://placehold.co/90x90/aabbcc/ffffff?text=${evennement}" alt="">
+            <h3 class="card-title">${evennement}</h3>
+            `;
             eventDiv.addEventListener('click', (event) => {
                 event.preventDefault();
                 displayFolder(date, evennement);
@@ -125,6 +116,7 @@ fetchSheetData().then(() => {
         document.getElementById('folder-list').innerHTML = '';
 
         for (var folderName of folder) {
+            folderName = folderName.replace('/view?usp=drive_link', '/preview')
             const folderDiv = document.createElement('a');
             folderDiv.href = folderName;
             folderDiv.target = "_blank"
