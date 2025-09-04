de07322c1331c1c03f4335c5265b7117490ca1aa
MaxBird18-le-vrai <million.maxime18@gmail.com>
2025-09-04 15:41:50 +0200

    Mise à jour des styles d'affichage des photos et amélioration de la logique de récupération des données Patch de la donnée en UTF-8

--- archives-photos.js
+++ archives-photos.js
@@ -4,17 +4,22 @@ var globalSheetData;
 // Dans loadSheetData, après avoir rempli globalSheetData :
 
 async function fetchSheetData() {
-  try {
-    const response = await fetch(proxyURL);
-    const data = await response.json();
-    const sheetContent = data.contents;
-    const base64Match = sheetContent.match(/data:text\/csv;base64,([^"]+)/);
-    let decodedCSV = base64Match && base64Match[1] ? atob(base64Match[1]) : sheetContent;
-    globalSheetData = parseCSV(decodedCSV);
-  } catch (error) {
-    console.error("Erreur lors de la récupération des données :", error);
-    globalSheetData = [];
-  }
+    try {
+        const response = await fetch(proxyURL,{mode: 'cors'});
+        const data = await response.json();
+        const sheetContent = data.contents;
+        const base64Match = sheetContent.match(/data:text\/csv;base64,([^"]+)/);
+        const base64Data = atob(base64Match[1]);
+        const uint8Array = new Uint8Array(base64Data.length);
+        for (let i = 0; i < base64Data.length; i++) {
+            uint8Array[i] = base64Data.charCodeAt(i);
+        }
+        const decodedCSV = new TextDecoder('utf-8').decode(uint8Array);
+        globalSheetData = parseCSV(decodedCSV);
+    } catch (error) {
+        console.error("Erreur lors de la récupération des données :", error);
+        globalSheetData = [];
+    }
 }
 
 function parseCSV(csv) {
@@ -26,7 +31,7 @@ function parseCSV(csv) {
       result.push({
         year: line[0].trim(),  // Première colonne = année
         event: line[1].trim(),  // Deuxième colonne = événement
-        folder: line[3].trim().replace('/view?usp=drive_link', '/preview'),  // Quatrième colonne = dossier
+        folder: line[3].trim().split('/')[5],  // Quatrième colonne = dossier
         default: line[5].trim()  // Sixième colonne = image par défaut
       });
     }
@@ -62,9 +67,6 @@ fetchSheetData().then(() => {
     }
 
     function displayEvents(date) {
-        // const events = [...new Set(globalSheetData
-        //     .filter(item => Number(item.year) === date)
-        //     .map(item => item.event))];
 
         const events = {};
 
@@ -87,6 +89,7 @@ fetchSheetData().then(() => {
         document.getElementById('dates-list').innerHTML = '';
         document.getElementById('events-list').innerHTML = '';
         document.getElementById('folder-list').innerHTML = '';
+
         
         for (const [evennement, imageLink] of Object.entries(events)) {
             const eventDiv = document.createElement('a');
@@ -94,14 +97,16 @@ fetchSheetData().then(() => {
             eventDiv.classList.add('card-role','photo');
             if (imageLink === "") {
                 eventDiv.innerHTML = `
-                <img class="card-image" src="https://placehold.co/90x90/aabbcc/ffffff?text=${evennement}" alt="">
-                <h3 class="card-title">${evennement}</h3>`
+                <div class="photo-event">
+                    <img src="https://placehold.co/90x90/aabbcc/ffffff?text=${evennement}" alt="">
+                </div>
+                <h3 class="card-title">${decodeURIComponent(evennement)}</h3>`
             } else {
                 eventDiv.innerHTML = `
-                <div>
-                    <iframe class="card-image" src="${imageLink}" scrolling="no" frameborder='0' alt=""></iframe>
+                <div class="photo-event">
+                    <img class="image-adaptee" src="https://drive.google.com/thumbnail?id=${imageLink}" alt=""></img>
                 </div>
-                <h3 class="card-title">${evennement}</h3>`
+                <h3 class="card-title">${decodeURIComponent(evennement)}</h3>`
             }
             eventDiv.addEventListener('click', (event) => {
                 event.preventDefault();
@@ -109,6 +114,22 @@ fetchSheetData().then(() => {
             })
             document.getElementById('events-list').appendChild(eventDiv);
         }
+
+        document.querySelectorAll('.image-adaptee').forEach(img => {
+            img.onload = function() {
+                if (this.naturalWidth > this.naturalHeight) {
+                // Image en paysage : priorité à la hauteur
+                this.style.height = '100%';
+                this.style.width = 'auto';
+                this.style.maxWidth = 'none';
+                } else {
+                // Image en portrait : priorité à la largeur
+                this.style.width = '100%';
+                this.style.height = 'auto';
+                this.style.maxHeight = 'none';
+                }
+            };
+        });
     }
 
     function displayFolder(date, event) {
@@ -126,11 +147,11 @@ fetchSheetData().then(() => {
 
         for (var folderName of folder) {
             const folderDiv = document.createElement('a');
-            folderDiv.href = folderName;
+            folderDiv.href = `https://drive.google.com/file/d/${folderName}/view?usp=drive_link`;
             folderDiv.target = "_blank"
             folderDiv.classList.add('card-role','photo');
             folderDiv.innerHTML = `
-            <iframe src="${folderName}" frameborder='0' height="200" allow="autoplay"></iframe>
+            <img src="https://drive.google.com/thumbnail?id=${folderName}"></img>
             `;
             document.getElementById('folder-list').appendChild(folderDiv);
         }
